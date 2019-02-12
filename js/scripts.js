a0552667e5aff3a88eea95737b83cb6aba1fba5b
IanSani <iansani259@gmail.com>
2019-02-12 11:59:33 +0300

    Add initial text

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+var add=function(number1,number2){
+  return number1+number2;
+};
+
+var number1=parseInt(prompt('Enter first number:'));
+var number2=parseInt(prompt("Enter second numnber:"));
+
+var result=(add(number1,number2));
+alert(result);
+
+var subtract=function(number1,number2){
+  return number1-number2;
+};
+var number1=parseInt(prompt("Enter first number:"));
+var number2=parseInt(prompt("Enter second number:"));
+
+var result=(subtract(number1,number2));
+alert(result);
+
+var multiply=function(number1,number2){
+  return number1*number2
+};
+var number1=parseInt(prompt("Enter first number"));
+var number2=parseInt(prompt("Enter second number"));
+var result=(multiply(number1,number2));
+alert(result);
+
+var divide=function(number1,number2){
+  return number1/number2
+};
+var number1=parseInt(prompt("Enter first number"));
+var number2=parseInt(prompt("Enter second number"));
+var result=(divide(number1,number2));
+alert(result);
